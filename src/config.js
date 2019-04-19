d96d025996de81f75a0716fc8c6370fa7be661dd
Geordie Connell <geordie.connell@gmail.com>
2019-04-19 10:08:45 -0400

    preparing for deployment

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
+'use strict';
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://zol@localhost/spaced-repetition',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/space-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
